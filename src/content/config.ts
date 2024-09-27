feda51b5cc5368617a9647ab12a82e617ccbe113
Joe Chelladurai <58997358+joe-chelladurai@users.noreply.github.com>
2024-09-27 13:15:12 -0600

    Update

--- src/content/config.ts
+++ src/content/config.ts
@@ -7,12 +7,11 @@ const docs = defineCollection({
     title: z.string(),
     description: z.string(),
     // Transform string to Date object
-    pubDate: z.coerce.date(),
+    pubDate: z.coerce.date().optional(),
     updatedDate: z.coerce.date().optional(),
     heroImage: z.string().optional(),
   }),
 });
-
 
 const reportCollection = defineCollection({
   schema: z.object({
